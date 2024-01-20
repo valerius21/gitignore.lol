11333eb070a9d447cce2381b59edd1b10894eddc
Valerius Mattfeld <mail@valerius.me>
2024-01-20 01:13:54 +0100

    Add .gitignore file and remove unused import

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"github.com/valerius21/gitignore.lol/pkg/repository"
 	"github.com/valerius21/gitignore.lol/pkg/web"
 )
 
 func main() {
-	// init logging
-	_ = repository.DefaultRepository
 	ws := web.DefaultWebServer
 
 	ws.App.Listen(":3000")
