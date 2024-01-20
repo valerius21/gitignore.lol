b96c6a6abacfb06f0ac13c11075cb5e78c335ca7
Valerius Mattfeld <mail@valerius.me>
2024-01-20 01:23:50 +0100

    Update port configuration in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/valerius21/gitignore.lol/pkg/web"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
 	ws := web.DefaultWebServer
 
-	ws.App.Listen(":3000")
+	ws.App.Listen(fmt.Sprintf(":%s", port))
 }
