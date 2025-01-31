b1f4b88a7a442ec3b1404afa93816967a1044f29
Valerius Mattfeld <mail@valerius.me>
2025-01-31 13:33:09 +0100

    refactor

--- pkg/lib/cli.go
+++ pkg/lib/cli.go
@@ -3,5 +3,5 @@ package lib
 var CLI struct {
 	Port           int    `help:"Port the server listens on." name:"port" default:"3000"`
 	BaseRepository string `help:"Gitignore repository where the .gitignore files are versioned." name:"repo" default:"https://github.com/github/gitignore.git"`
-	TmpLocation    string `help:"Directory of the temporarly stored files" name:"tmp-dir" default:"./store" type:"existingdir"`
+	SubmodulePath  string `help:"Location of the locally stored gitignore submodule" name:"submodule-path" default:"./store" type:"path"`
 }
