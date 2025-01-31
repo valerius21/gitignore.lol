16ddeb7c3e62f26499edca44762ed6ca8cc72c2e
Valerius Mattfeld <mail@valerius.me>
2025-01-31 15:27:52 +0100

    update gitrunner

--- pkg/lib/cli.go
+++ pkg/lib/cli.go
@@ -4,4 +4,5 @@ var CLI struct {
 	Port           int    `help:"Port the server listens on." name:"port" default:"3000"`
 	BaseRepository string `help:"Gitignore repository where the .gitignore files are versioned." name:"repo" default:"https://github.com/github/gitignore.git"`
 	ClonePath      string `help:"Location of the locally stored gitignore repository" name:"clone-path" default:"./store" type:"path"`
+	UpdateInterval int    `help:"Interval (seconds) in which the linked repository gets updated" name:"fetch-interval" default:"300"`
 }
--- pkg/lib/git-runner.go
+++ pkg/lib/git-runner.go
@@ -1,1 +1,65 @@
 package lib
+
+import (
+	"os"
+
+	"github.com/go-git/go-git/v5"
+)
+
+type GitRunner struct {
+	origin        string
+	localPath     string
+	fetchInterval int
+}
+
+func New(origin, path string, fetchInterval int) *GitRunner {
+	return &GitRunner{
+		origin:        origin,
+		localPath:     path,
+		fetchInterval: fetchInterval,
+	}
+}
+
+// Init checks, if the local repository exists, if not clones it.
+// If it does exist, it updates it.
+func (gr *GitRunner) Init() error {
+	// Check if directory exists, if not clone the repo
+	if _, err := os.Stat(gr.localPath); os.IsNotExist(err) {
+		// TODO: log
+		_, err = git.PlainClone(gr.localPath, false, &git.CloneOptions{
+			URL: gr.origin,
+		})
+
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	// update repo
+
+	repo, err := git.PlainOpen(gr.localPath)
+	if err != nil {
+		return err
+	}
+
+	wt, err := repo.Worktree()
+	if err != nil {
+		return err
+	}
+
+	err = wt.Pull(&git.PullOptions{RemoteName: "origin"})
+	if err != nil {
+		return err
+	}
+
+	// log latest commit hash that was pulled
+	_, err = repo.Head()
+	if err != nil {
+		return err
+	}
+
+	// TODO: log ref
+
+	return nil
+}
