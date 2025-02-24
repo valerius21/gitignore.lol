eea08ca879c96daa461a0fbcbcf67535811a034c
Valerius Mattfeld <mail@valerius.me>
2025-02-24 17:00:48 +0100

    update import

--- web/landing-page/app/layout.tsx
+++ web/landing-page/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Image from "next/image";
 import { Toaster } from 'sonner'
 
 const geistSans = Geist({
