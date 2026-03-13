061bc5e18caf5507b218f9c7430d9ce5b7ebe0a7
Valerius Mattfeld <git@valerius.me>
2026-03-13 17:37:31 +0100

    chore: upgrade Next.js to stable, remove package-lock.json

--- web/landing-page/components/search-box.tsx
+++ web/landing-page/components/search-box.tsx
@@ -1,33 +1,32 @@
-'use client'
-import { MultiSelect } from "@/components/ui/multi-select";
-import { selectionAtom } from "@/lib/stores";
+"use client";
+
 import { useQuery } from "@tanstack/react-query";
 import { useAtom } from "jotai";
 
-export function SearchBox() {
-  const [selection, setSelection] = useAtom(selectionAtom)
-  const { data, isLoading } = useQuery<{ files: string[] }>({
-    queryKey: ['ignore-list'],
-    queryFn: () => fetch('/api/list').then(res => res.json()),
-    initialData: ({
-      files: ['python', 'c']
-    })
-  })
+import { MultiSelect } from "@/components/ui/multi-select";
+import { selectionAtom } from "@/lib/stores";
 
-  return (
-    <>
-      <MultiSelect
-        options={data?.files.map(s => ({
-          label: s,
-          value: s
-        }))}
-        onValueChange={setSelection}
-        defaultValue={data?.files.slice(0, 3).map(s => s)}
-        placeholder="Select a template ..."
-        variant={'inverted'}
-        maxCount={100}
-      >
-      </MultiSelect>
-    </>
-  )
+export function SearchBox() {
+	const [, setSelection] = useAtom(selectionAtom);
+	const { data } = useQuery<{ files: string[] }>({
+		queryKey: ["ignore-list"],
+		queryFn: () => fetch("/api/list").then((res) => res.json()),
+		initialData: {
+			files: ["python", "c"],
+		},
+	});
+
+	return (
+		<MultiSelect
+			options={data?.files.map((s) => ({
+				label: s,
+				value: s,
+			}))}
+			onValueChange={setSelection}
+			defaultValue={data?.files.slice(0, 3).map((s) => s)}
+			placeholder="Select a template ..."
+			variant={"inverted"}
+			maxCount={100}
+		></MultiSelect>
+	);
 }
